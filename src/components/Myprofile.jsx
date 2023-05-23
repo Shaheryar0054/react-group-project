2b44fd43190f6151ae60b328abaee863cd9c53cd
shaheryar abid <sharyar0310@gmail.com>
2023-05-23 20:47:36 +0500

    Added react Routing for navigation

---
+++ src/components/Myprofile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Myprofile() {
+  return (
+    <div>
+      <h1>Myprofile Page</h1>
+    </div>
+  );
+}
+
+export default Myprofile;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,26 +1,51 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, NavLink, Routes, Route,
+} from 'react-router-dom';
 import planet from './images/planet.png';
-
-import './NavBar.css';
+import Rockets from './Rockets';
+import Missions from './Missions';
+import Myprofile from './Myprofile';
+import '../styles/NavBar.css';
 
 const Nav = () => (
-  <>
-    <div className="NavDiv">
-      <div className="NavImg">
-        <img src={planet} width="50px" height="50px" alt="" />
-        <div className="TitleNav"> Space travelers Hubs</div>
+  <Router>
+    <header id="header-container">
+      <div className="NavDiv">
+        <div className="NavImg">
+          <img src={planet} width="50px" height="50px" alt="" />
+          <h2>Space travelers Hubs</h2>
+        </div>
+
+        <nav id="nav-container">
+          <ul>
+            <li>
+              <NavLink to="/rockets" activeClassName="active">
+                Rockets
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/missions" activeClassName="active">
+                Missions
+              </NavLink>
+            </li>
+            <hr className="pages-line" />
+            <li>
+              <NavLink to="/profile" activeClassName="active">
+                My Profile
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <nav>
-        <ul>
-          <li>Rokets</li>
-          <li>Missions</li>
-          <div className="Interline" />
-          <li>My Profile</li>
-        </ul>
-      </nav>
-    </div>
-    <hr />
-  </>
+      <hr />
+    </header>
+    <Routes>
+      <Route path="/rockets" element={<Rockets />} />
+      <Route path="/missions" element={<Missions />} />
+      <Route path="/profile" element={<Myprofile />} />
+    </Routes>
+  </Router>
 );
 
 export default Nav;
